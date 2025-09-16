efa3caae513c6b8bcddb6246dbd5afd1ed961976
Stacy <stacyxberlin@gmail.com>
2025-09-16 15:21:52 +0200

    try to fix cors

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -47,14 +47,14 @@ export const setupCookies = (refreshToken, sid, refreshValidUntil, res)=>{
     res.cookie('refreshToken', refreshToken,{
         httpOnly: true,
         expires: refreshValidUntil,
-        sameSite: 'none',
-        secure: true,
+        sameSite: 'lax',
+        secure: false,
     })
     res.cookie('sid', sid, {
         httpOnly: true,
         expires: refreshValidUntil,
-        sameSite: 'none',
-        secure: true,
+        sameSite: 'lax',
+        secure: false,
     })
 }
 
